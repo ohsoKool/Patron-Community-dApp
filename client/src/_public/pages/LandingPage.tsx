84b2d3751e26155d9be4f8cfbd81d1ecc13f250e
SneakyBuzZ <kaushikx304@gmail.com>
2024-10-31 20:31:52 +0530

    Reverted changes

--- client/src/_public/pages/LandingPage.tsx
+++ client/src/_public/pages/LandingPage.tsx
@@ -1,13 +1,9 @@
 import HeroSection from '@/components/public/HeroSection';
-import { FeaturesSectionDemo } from '@/components/public/FeatureSection';
-import { OrbitingCirclesDemo } from '@/components/public/WalletSection';
 
 const LandingPage = () => {
   return (
     <>
       <HeroSection />
-      <FeaturesSectionDemo/>
-      <OrbitingCirclesDemo/>
     </>
   );
 };
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,114 +1,118 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	//darkMode: ["class"],
-	content: [
-	  './pages/**/*.{ts,tsx}',
-	  './components/**/*.{ts,tsx}',
-	  './app/**/*.{ts,tsx}',
-	  './src/**/*.{ts,tsx}',
-	], 
-	prefix: "",
-	theme: {
-	  container: {
-		center: true,
-		padding: "2rem",
-		screens: {
-		  "2xl": "1400px",
-		},
-	  },
-	  extend: {
-		fontFamily: {
-		  'audio-wide': ["Audiowide", "sans-serif"],
-		  'fira-code': ["Fira Code"],
-		  'changa': ["Changa", 'sans-serif']
-		},
-		colors: {
-  
-		  // GENERAL COLORS 
-		  PATRON_BLACK: "#030303",
-		  PATRON_DARK_GRAY: '#101010',
-		  PATRON_LIGHT_GRAY: '#171717',
-		  PATRON_CYAN: '#009BBE',
-		  PATRON_GREEN: '#00CA87',
-		  PATRON_PURPLE: '#7200BE',
-		  PATRON_YELLOW: '#CBB300',
-  
-  
-  
-		  // BORDER COLORS
-		  PATRON_BORDER_COLOR: '#2A2A2A',
-  
-		  // TEXT COLORS 
-		  PATRON_TEXT_WHITE_SECONDARY: '#9D9D9D',
-		  PATRON_TEXT_WHITE_PRIMARY: '#C5C5C5',
-  
-  
-  
-		  border: "hsl(var(--border))",
-		  input: "hsl(var(--input))",
-		  ring: "hsl(var(--ring))",
-		  background: "hsl(var(--background))",
-		  foreground: "hsl(var(--foreground))",
-		  primary: {
-			DEFAULT: "hsl(var(--primary))",
-			foreground: "hsl(var(--primary-foreground))",
-		  },
-		  secondary: {
-			DEFAULT: "hsl(var(--secondary))",
-			foreground: "hsl(var(--secondary-foreground))",
-		  },
-		  destructive: {
-			DEFAULT: "hsl(var(--destructive))",
-			foreground: "hsl(var(--destructive-foreground))",
-		  },
-		  muted: {
-			DEFAULT: "hsl(var(--muted))",
-			foreground: "hsl(var(--muted-foreground))",
-		  },
-		  accent: {
-			DEFAULT: "hsl(var(--accent))",
-			foreground: "hsl(var(--accent-foreground))",
-		  },
-		  popover: {
-			DEFAULT: "hsl(var(--popover))",
-			foreground: "hsl(var(--popover-foreground))",
-		  },
-		  card: {
-			DEFAULT: "hsl(var(--card))",
-			foreground: "hsl(var(--card-foreground))",
-		  },
-		},
-		borderRadius: {
-		  lg: "var(--radius)",
-		  md: "calc(var(--radius) - 2px)",
-		  sm: "calc(var(--radius) - 4px)",
-		},
-		keyframes: {
-		  orbit: {
-			"0%": {
-			  transform:
-				"rotate(0deg) translateY(calc(var(--radius) * 1px)) rotate(0deg)",
-			},
-			"100%": {
-			  transform:
-				"rotate(360deg) translateY(calc(var(--radius) * 1px)) rotate(-360deg)",
-			},
-		  },
-		  "accordion-down": {
-			from: { height: "0" },
-			to: { height: "var(--radix-accordion-content-height)" },
-		  },
-		  "accordion-up": {
-			from: { height: "var(--radix-accordion-content-height)" },
-			to: { height: "0" },
-		  },
-		},
-		animation: {
-		  "accordion-down": "accordion-down 0.2s ease-out",
-		  "accordion-up": "accordion-up 0.2s ease-out",
-		  orbit: "orbit calc(var(--duration)*1s) linear infinite",
-		},
-	  },
-	},
-	plugins: [require("tailwindcss-animate")],
-  }
+  darkMode: ["class"],
+  content: [
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+  ],
+  prefix: "",
+  theme: {
+  	container: {
+  		center: 'true',
+  		padding: '2rem',
+  		screens: {
+  			'2xl': '1400px'
+  		}
+  	},
+  	extend: {
+  		fontFamily: {
+  			'audio-wide': ["Audiowide", "sans-serif"],
+  			'fira-code': ["Fira Code"],
+  			changa: ["Changa", 'sans-serif']
+  		},
+  		colors: {
+  			PATRON_BLACK: '#030303',
+  			PATRON_DARK_GRAY: '#101010',
+  			PATRON_LIGHT_GRAY: '#171717',
+  			PATRON_CYAN: '#009BBE',
+  			PATRON_GREEN: '#00CA87',
+  			PATRON_PURPLE: '#7200BE',
+  			PATRON_YELLOW: '#CBB300',
+  			PATRON_BORDER_COLOR: '#2A2A2A',
+  			PATRON_TEXT_WHITE_SECONDARY: '#9D9D9D',
+  			PATRON_TEXT_WHITE_PRIMARY: '#C5C5C5',
+  			border: 'hsl(var(--border))',
+  			input: 'hsl(var(--input))',
+  			ring: 'hsl(var(--ring))',
+  			background: 'hsl(var(--background))',
+  			foreground: 'hsl(var(--foreground))',
+  			primary: {
+  				DEFAULT: 'hsl(var(--primary))',
+  				foreground: 'hsl(var(--primary-foreground))'
+  			},
+  			secondary: {
+  				DEFAULT: 'hsl(var(--secondary))',
+  				foreground: 'hsl(var(--secondary-foreground))'
+  			},
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			muted: {
+  				DEFAULT: 'hsl(var(--muted))',
+  				foreground: 'hsl(var(--muted-foreground))'
+  			},
+  			accent: {
+  				DEFAULT: 'hsl(var(--accent))',
+  				foreground: 'hsl(var(--accent-foreground))'
+  			},
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			card: {
+  				DEFAULT: 'hsl(var(--card))',
+  				foreground: 'hsl(var(--card-foreground))'
+  			}
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		},
+  		keyframes: {
+  			'accordion-down': {
+  				from: {
+  					height: '0'
+  				},
+  				to: {
+  					height: 'var(--radix-accordion-content-height)'
+  				}
+  			},
+  			'accordion-up': {
+  				from: {
+  					height: 'var(--radix-accordion-content-height)'
+  				},
+  				to: {
+  					height: '0'
+  				}
+  			},
+  			'accordion-down': {
+  				from: {
+  					height: '0'
+  				},
+  				to: {
+  					height: 'var(--radix-accordion-content-height)'
+  				}
+  			},
+  			'accordion-up': {
+  				from: {
+  					height: 'var(--radix-accordion-content-height)'
+  				},
+  				to: {
+  					height: '0'
+  				}
+  			}
+  		},
+  		animation: {
+  			'accordion-down': 'accordion-down 0.2s ease-out',
+  			'accordion-up': 'accordion-up 0.2s ease-out',
+  			'accordion-down': 'accordion-down 0.2s ease-out',
+  			'accordion-up': 'accordion-up 0.2s ease-out'
+  		}
+  	}
+  },
+  plugins: [require("tailwindcss-animate")],
+}
