5a27a2011b151931a388392fa5b000f1c2f6a91f
SneakyBuzZ <kaushikx304@gmail.com>
2024-10-31 20:29:44 +0530

    Added feature section

--- client/src/components/public/FeatureSection.tsx
+++ client/src/components/public/FeatureSection.tsx
@@ -1,11 +1,10 @@
-import { useId } from 'react';
+//import React from "react";
+import { useId } from "react";
 
 export function FeaturesSectionDemo() {
   return (
     <div className="py-20 lg:py-8 mb-0 ">
-      <h1 className="text-center text-2xl mt-28 mb-12 font-fira-code">
-        Discover What Sets Us Apart
-      </h1>
+        <h1 className="text-center text-2xl mt-28 mb-12 font-fira-code">Discover What Sets Us Apart</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-10 md:gap-10 max-w-5xl mx-auto">
         {grid.map((feature) => (
           <div
@@ -23,43 +22,53 @@ export function FeaturesSectionDemo() {
         ))}
       </div>
     </div>
+
   );
 }
 
 const grid = [
-  {
-    title: 'Decentralized Identity Management',
-    description:
-      'Foster dynamic and secure interactions within a decentralized, blockchain-powered community.',
-  },
-  {
-    title: 'End-to-End Encryption',
-    description:
-      'Ensure user privacy with encrypted communication and data-sharing protocols throughout the platform.',
-  },
-  {
-    title: 'Immutable Record Keeping',
-    description:
-      'Store all interactions and transactions on a tamper-proof blockchain ledger for complete transparency.',
-  },
-  {
-    title: 'Smart Contract Integration',
-    description:
-      'Automate and streamline community governance and transactions with self-executing smart contracts.',
-  },
-  {
-    title: 'Tokenized Reputation System',
-    description:
-      'Reward positive contributions with tokens, allowing users to build trust and influence within the community.',
-  },
-  {
-    title: 'Privacy-Focused Data Sharing',
-    description:
-      'Provide users with full control over their data, enabling secure and permission-based sharing.',
-  },
-];
+    {
+      title: "Decentralized Identity Management",
+      description:
+        "Foster dynamic and secure interactions within a decentralized, blockchain-powered community.",
+    },
+    {
+      title: "End-to-End Encryption",
+      description:
+        "Ensure user privacy with encrypted communication and data-sharing protocols throughout the platform.",
+    },
+    {
+      title: "Immutable Record Keeping",
+      description:
+        "Store all interactions and transactions on a tamper-proof blockchain ledger for complete transparency.",
+    },
+    {
+      title: "Smart Contract Integration",
+      description:
+        "Automate and streamline community governance and transactions with self-executing smart contracts.",
+    },
+    {
+      title: "Tokenized Reputation System",
+      description:
+        "Reward positive contributions with tokens, allowing users to build trust and influence within the community.",
+    },
+    {
+      title: "Privacy-Focused Data Sharing",
+      description:
+        "Provide users with full control over their data, enabling secure and permission-based sharing.",
+    },
+    
+  ];
+  
+  
 
-export const Grid = ({ pattern, size }: { pattern?: number[][]; size?: number }) => {
+export const Grid = ({
+  pattern,
+  size,
+}: {
+  pattern?: number[][];
+  size?: number;
+}) => {
   const p = pattern ?? [
     [Math.floor(Math.random() * 4) + 7, Math.floor(Math.random() * 6) + 1],
     [Math.floor(Math.random() * 4) + 7, Math.floor(Math.random() * 6) + 1],
@@ -100,7 +109,12 @@ export function GridPattern({ width, height, x, y, squares, ...props }: any) {
           <path d={`M.5 ${height}V.5H${width}`} fill="none" />
         </pattern>
       </defs>
-      <rect width="100%" height="100%" strokeWidth={0} fill={`url(#${patternId})`} />
+      <rect
+        width="100%"
+        height="100%"
+        strokeWidth={0}
+        fill={`url(#${patternId})`}
+      />
       {squares && (
         <svg x={x} y={y} className="overflow-visible">
           {squares.map(([x, y]: any) => (
